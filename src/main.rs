36fd2d787233dadbc622c82a344128ec81ac97e1
Arulselvan Madhavan <arulselvan1234@gmail.com>
2019-04-27 07:54:20 -0700

    Getting started with hello world

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+extern crate actix_web;
+extern crate listenfd;
+
+use actix_web::{server, App, HttpRequest};
+use listenfd::ListenFd;
+
+fn index(_req: &HttpRequest) -> &'static str {
+    "Hello world by Arul!"
+}
+
+fn main() {
+    let mut listenfd = ListenFd::from_env();
+    let mut server = server::new(|| App::new().resource("/", |r| r.f(index)));
+    server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
+        server.listen(l)
+    } else {
+        server.bind("127.0.0.1:3000").unwrap()
+    };
+    server.run()
+}
